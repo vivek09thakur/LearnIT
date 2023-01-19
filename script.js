4540cba4eab4a06b6c1aa4fb643b3fead7a7182b
Vivek Thakur <vivekthakurcse779@gmail.com>
2023-01-19 22:58:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-
 AOS.init();
 window.addEventListener("scroll", function () {
-    const header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0);
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
 });
-
-
 
 //Box1 for Semester 1
 let box1 = document.getElementById("box1");
@@ -16,7 +13,6 @@ function closeBox1() {
     box1.classList.remove("open-box1");
 }
 
-
 //Box2 for Semester 2
 let box2 = document.getElementById("box2");
 function openBox2() {
@@ -25,7 +21,6 @@ function openBox2() {
 function closeBox2() {
     box2.classList.remove("open-box2");
 }
-
 
 //Box3 for Semester 3
 let box3 = document.getElementById("box3");
@@ -36,7 +31,6 @@ function closeBox3() {
     box3.classList.remove("open-box3");
 }
 
-
 //Box 4 for CSE semester 4
 let box4 = document.getElementById("box4");
 function openBox4() {
@@ -45,7 +39,6 @@ function openBox4() {
 function closeBox4() {
     box4.classList.remove("open-box4");
 }
-
 
 //Box 5 for CSE semester 5
 let box5 = document.getElementById("box5");
@@ -64,14 +57,13 @@ function closeBox6() {
     box6.classList.remove("open-box6");
 }
 
-
 $("#inline-popups").magnificPopup({
     delegate: "a",
     removalDelay: 500, //delay removal by X to allow out-animation
     callbacks: {
         beforeOpen: function () {
             this.st.mainClass = this.st.el.attr("data-effect");
-        }
+        },
     },
-    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+    midClick: true, // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
 });
